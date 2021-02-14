76ec56bc00094f216955fd013e672ff2681c66c6
rdcelestial <rdcelestial@gmail.com>
2021-02-14 09:09:27 +0800

    test
    test

---
+++ expo-first-app/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Sample App React NAtive app uisng expo ')
+  return (
+    <View style={styles.container}>
+      <Text>{outputText} </Text>
+      <Button title="Change Text" onPress={() =>setOutputText('The Text Change')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
